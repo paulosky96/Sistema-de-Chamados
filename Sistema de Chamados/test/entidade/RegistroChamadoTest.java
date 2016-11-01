322c8fd61495d1acf7fd34391d11d34a489504d8
157T <>
2016-11-01 19:41:26 -0200

    JUnit-

--- Sistema de Chamados/test/entidade/RegistroChamadoTest.java
+++ Sistema de Chamados/test/entidade/RegistroChamadoTest.java
@@ -208,7 +208,7 @@ public class RegistroChamadoTest {
 //        Tecnico result = instance.getTecnico();
 //        assertEquals(expResult, result);
 //        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+//        fail("The test case is a prototype.")
 //    }
 //
 //    /**
